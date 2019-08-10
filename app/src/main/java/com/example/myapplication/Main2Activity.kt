bcdff1ef7b48eb2e613c2b17f64b7c9901925a50
zhouzhanke <allen.zhouzhanke@gmail.com>
2019-08-10 11:14:23 -0700

    kotlin sample

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val bundle: Bundle? =intent.extras
+        val msg = bundle!!.getString("msg")
+
+
+        textView.text = msg
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}
